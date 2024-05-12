49f7ae65f4fdf5fe3e98a58bb1e2a5b236556341
Xushnudkom <xushnudkomilovv5681@gmail.com>
2024-05-12 08:53:03 +0500

    repre

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById('click_exit').addEventListener('click', function() {
+    document.getElementById('travel_box').style.display = 'block';
+    document.getElementById('travel_box').style.display = 'none';
+});
+
+document.getElementById('button_one').addEventListener('click', function() {
+    document.getElementById('travel_box').style.display = 'none';
+    document.getElementById('travel_box').style.display = 'block';
+});
